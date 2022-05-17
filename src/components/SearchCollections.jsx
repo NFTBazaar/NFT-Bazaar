90d088e8ab5ab87db57dd7dc0b10bc2e5e472449
yogirajgutte <yogiraj.gutte@udchalo.com>
2022-05-17 12:06:08 +0530

    commit

---
+++ src/components/SearchCollections.jsx
@@ -0,0 +1,39 @@
+import { Select } from 'antd';
+import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
+import { getCollectionsByChain } from "helpers/collections";
+import "../style.css"
+
+function SearchCollections({setInputValue}){
+    const { Option } = Select;
+    const { chainId } = useMoralisDapp();
+    const NFTCollections = getCollectionsByChain(chainId);
+    
+    
+
+    function onChange(value) {
+        setInputValue(value);
+    }
+
+    return (
+        <>
+        <Select
+            showSearch
+            style={{width: "1000px",
+                    marginLeft: "20px",
+                    backgroundColor:"#064663",
+                }}
+            placeholder="Find a Collection"
+            optionFilterProp="children"
+            onChange={onChange}
+        >   
+        {NFTCollections && 
+            NFTCollections.map((collection, i) => 
+            <Option value={collection.addrs} key= {i}>{collection.name}</Option>
+            )
+            }   
+        </Select>
+            
+        </>
+    )
+}
+export default SearchCollections;
