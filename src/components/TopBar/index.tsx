7df30135bafb7c25d844554c7a0286cc0f44466c
yogirajgutte <yogiraj.gutte@udchalo.com>
2022-05-18 18:38:42 +0530

    added environment variables

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+import ConnectButton from "../ConnectButton";
+import NavBar from "./NavBar";
+import logo from "../../static/logo.jpg"
+
+const TopBar = () => {
+  return (
+    <div className="fixed top-0 w-full nav-bar">
+      <div className="relative flex w-full items-center px-4  py-4 shadow">
+        <Link href="/">
+          <Image src={logo} alt="NFT Bazaar" width={"200px"} height={"100px"} />
+        </Link>
+        <div className="flex-grow">
+          <NavBar />
+        </div>
+        <ConnectButton />
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
