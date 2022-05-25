ac416064e28020cf4769619985ae29649ae87f54
Aasit Vora <aasitvora1999@gmail.com>
2022-05-25 09:22:45 +0530

    Minor UI changes

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import ConnectButton from "../ConnectButton";
 import NavBar from "./NavBar";
-import logo from "../../static/nftbazaar-logo.png"
+import logo from "../../static/nftbazaar.png"
 
 const TopBar = () => {
   return (
